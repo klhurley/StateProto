d82d0f3af63a126d39df8b7e211656d349a50252
Kenneth Hurley <kenneth.hurley@sonyericsson.com>
2012-01-12 13:41:32 -0800

    Forgot to checkin the setting file

---
+++ MurphyPA/Modelling/H2D/src/MurphyPA.H2D.TestApp/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace MurphyPA.H2D.TestApp.Properties
+{
+    public sealed partial class Settings
+    {
+        private static ApplicationSettingsBase defaultInstance;
+        
+        public Settings(ApplicationSettingsBase srcSettings)
+        {
+            defaultInstance = srcSettings;
+        }
+
+        public static ApplicationSettingsBase Default 
+        {
+            get 
+            {
+                return defaultInstance;
+            }
+        }
+
+    }
+}
